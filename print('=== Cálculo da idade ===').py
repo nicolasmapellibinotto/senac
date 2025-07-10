c700abbd01be36c9e492cbd8abd5536ec7ef01a3
nicolasmapellibinotto <04477344074@senacrs.edu.br>
2025-07-10 09:03:41 -0300

    Add files via upload

---
+++ print('=== Cálculo da idade ===').py
@@ -0,0 +1,5 @@
+print('=== Cálculo da idade ===')
+ano_atual = 2025
+ano_nacimento = int(input('Digite o ano do nacimento:'))
+idade = ano_atual -ano_nacimento
+print('A sua idade é ' +idade)
---
+++ variaveis.py
@@ -0,0 +1,11 @@
+print("Bom dia !!")
+nome = 'Nicolas'
+print(nome)
+idade = 17
+print(idade)
+salario_minimo = 1450.99
+print(salario_minimo)
+ativo = True  
+print(ativo)
+inativo = True 
+print(inativo)
